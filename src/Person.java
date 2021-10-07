6e676cbbadb61a7c8b9cac2304d95d5720796cf0
Alex <aandreba@gmail.com>
2021-10-07 17:00:23 +0200

    Person.java created

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,38 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
 public class Person {
+    final public String firstName, lastName, dni;
+    final public LocalDate birthDate;
+
+    public Person (String firstName, String lastName, String dni, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dni = dni;
+        this.birthDate = birthDate;
+    }
+
+    // METHODS
+    public String getFullName () {
+        return this.firstName + ' ' + this.lastName;
+    }
+
+    public Period getAge () {
+        return Period.between(LocalDate.now(), this.birthDate);
+    }
+
+    public int getYears () {
+        return getAge().getYears();
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dni='" + dni + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
