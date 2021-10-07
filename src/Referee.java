0ede5b492bcde562f0ff013fbe3d81c1fff85ec3
khadijara <91148260+khadijara@users.noreply.github.com>
2021-10-07 17:23:50 +0200

    Referee.java class

--- src/Referee.java
+++ src/Referee.java
@@ -1,2 +1,26 @@
-public class Referee {
+public class Referee  {
+
+    private static char takeDecision;
+
+
+    /**
+     * Method: showingCard
+     * This method is showing the referee decision to the player.
+     * If Referee take decision (Y) it's means Yellow Card and it's a warning.
+     * If he is showing (R) it's means Red Card and it's  a Error.
+     */
+
+    public static  void showingCard(){
+        if (takeDecision ==  'Y') {
+            System.out.println("WARNING: It's a warning for a Player.");
+        } else if (takeDecision == 'R') {
+            System.out.println("The Player has to go from Game.");
+        }
+    }
+
+
+    public static void setTakeDecision(char takeDecision) {
+        Referee.takeDecision = takeDecision;
+    }
 }
+
